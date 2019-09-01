2e3b96e34503afe31b368c7259732b1da757e043
Josh de Kock <josh@itanimul.li>
2019-09-01 14:04:56 +0100

    cleanup spMFSynchronizeClasses

--- Connector/30_DataExchange/30.611.spMFSynchronizeClasses.sql
+++ Connector/30_DataExchange/30.611.spMFSynchronizeClasses.sql
@@ -55,24 +55,26 @@ ALTER PROCEDURE [dbo].[spMFSynchronizeClasses]
       , @Out [NVARCHAR](MAX) OUTPUT
       , @IsUpdate SMALLINT = 0
       )
-AS 
-
-    
+AS
 /*rST**************************************************************************
 
 ======================
 spMFSynchronizeClasses
 ======================
 
+Return
+  - 1 = Success
+  - -1 = Error
 Parameters
-  @VaultSettings
+  @VaultSettings nvarchar(4000)
     - use fnMFVaultSettings()
-  @Debug (optional)
+  @Debug smallint (optional)
     - Default = 0
     - 1 = Standard Debug Mode
-  @Out (Output)
+    - 101 = Advanced Debug Mode
+  @Out nvarchar(max) (output)
     - XML result
-  @IsUpdate (Optional)
+  @IsUpdate smallint (optional)
     - Default = 0
     - 1 = Push updates from SQL to M-Files
 
@@ -88,6 +90,7 @@ Changelog
 ==========  =========  ========================================================
 Date        Author     Description
 ----------  ---------  --------------------------------------------------------
+2019-08-30  JC         Added documentation
 2018-04-04  DEV2       Added Module validation code
 2017-12-3   LC         Prevent MFID -100 assignements to be included in update
 2017-09-11  LC         Resolve issue with constraints
