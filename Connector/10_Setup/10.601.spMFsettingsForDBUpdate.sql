5c7bf4d9136361b68263efb2af35866ffd5c9387
Josh de Kock <josh@itanimul.li>
2019-09-03 10:00:22 +0100

    improve spMFsettingsForDBUpdate formatting

--- Connector/10_Setup/10.601.spMFsettingsForDBUpdate.sql
+++ Connector/10_Setup/10.601.spMFsettingsForDBUpdate.sql
@@ -71,40 +71,40 @@ Return
   - -1 = Error
 Parameters
   @MFInstallationPath nvarchar(128)
-    Path where M-Files is installed on SQL server
-    Default assigned by installer 'C:\Program Files\M-Files'
+    - Path where M-Files is installed on SQL server
+    - Default assigned by installer 'C:\Program Files\M-Files'
   @MFilesVersion nvarchar(128)
-    M-Files version deployed on SQL server
-    Format '11.2.4320.51'
+    - M-Files version deployed on SQL server
+    - Format '11.2.4320.51'
   @AssemblyInstallationPath nvarchar(128)
-    Path where the Laminin Assemblies have been copied to.
-    Default assigned by installer 'C:\MFSQL\Assemblies' 
+    - Path where the Laminin Assemblies have been copied to.
+    - Default assigned by installer 'C:\MFSQL\Assemblies'
   @SQLConnectorLogin nvarchar(128)
-    SQL login user for the Connector
-....Default assigned by installer 'MFSQLConnect'
+    - SQL login user for the Connector
+    - Default assigned by installer 'MFSQLConnect'
   @UserRole nvarchar(128)
-    Role for SQL user
-    Default assigned by installer 'AppUserRole'
+    - Role for SQL user
+    - Default assigned by installer 'AppUserRole'
   @SupportEmailAccount nvarchar(128)
-    Email to receive system error emails
-    Default assigned by installer 'support@lamininsolutions.com', semi colon separated for multiple emails
-    Depended on installing Database Mail
+    - Email to receive system error emails
+    - Default assigned by installer 'support@lamininsolutions.com', semi colon separated for multiple emails
+    - Depended on installing Database Mail
   @EmailProfile nvarchar(128)
-    DBMail profile to be used for emails
-    Default assigned by installer 'LSEmailProfile' 
-    Depended on Database Mail installation
+    - DBMail profile to be used for emails
+    - Default assigned by installer 'LSEmailProfile'
+    - Depended on Database Mail installation
   @DetailLogging nvarchar(128)
-    1 = include detail logging
-    Default of 1 assigned by installer
+    - 1 = include detail logging
+    - Default of 1 assigned by installer
   @DBName nvarchar(128)
-    Connector database name
-    Assigned by installer
+    - Connector database name
+    - Assigned by installer
   @RootFolder nvarchar(128)
-    Base folder for exporting of files
-    Default assigned by installer 'C:\MFSQL\FileExport'
+    - Base folder for exporting of files
+    - Default assigned by installer 'C:\MFSQL\FileExport'
   @FileTransferLocation nvarchar(128)
-    Base folder for importing files
-    Default assigned by installer 'C:\MFSQL\FileImport'
+    - Base folder for importing files
+    - Default assigned by installer 'C:\MFSQL\FileImport'
   @Debug smallint (optional)
     - Default = 0
     - 1 = Standard Debug Mode
@@ -113,23 +113,28 @@ Parameters
 
 Purpose
 =======
+
 Procedure to allow updating of specific settings in the MFSettings table
 
 Additional Info
 ===============
+
 Include only the the items that need to be changed.
 Refer to MFSettings for more information on the use of the settings.
 
 Prerequisites
 =============
-  Email functions is dependent on Database Mail being installed.
+
+Email functions is dependent on Database Mail being installed.
 
 Warnings
 ========
+
 Settings have an impact throughout the Connector.
 
 Examples
 ========
+
 .. code:: sql
 
     EXEC [spMFSettingsForDBUpdate]  @SupportEmailAccount = 'YourEmailAccount'
@@ -172,11 +177,11 @@ Changelog
 Date        Author     Description
 ----------  ---------  --------------------------------------------------------
 2019-08-30  JC         Added documentation
-2016-8-22   LC         Change Settings index
-2017-9-2    LC         Add RootFolder setting
+2018-04-28  LC         Add user message setting; vault structure setting
+2018-02-16  LC         Add file import and export change setting
 2017-11-23  LC         Resolve bug for missing value
-2018-2-16   LC         Add file import and export change setting
-2018-4-28   LC         Add user message setting; vault structure setting
+2017-09-02  LC         Add RootFolder setting
+2016-08-22  LC         Change Settings index
 ==========  =========  ========================================================
 
 **rST*************************************************************************/
