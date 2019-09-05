b0ffabc4a7645e0b503c5672fb670d4e628d8242
Josh de Kock <josh@itanimul.li>
2019-09-05 17:54:01 +0100

    add spMFDeleteAdhocProperty documentation

--- Connector/35_Integration/35.604.spMFDeleteAdhocProperty.sql
+++ Connector/35_Integration/35.604.spMFDeleteAdhocProperty.sql
@@ -9,11 +9,7 @@ EXEC [Setup].[spMFSQLObjectsControl] @SchemaName = N'dbo'
                                     ,@Object_Release = '4.3.09.48'
                                     -- varchar(50)
                                     ,@UpdateFlag = 2;
--- smallint
-/*
-2018-4-25 LC fix bu to pick up both ID column and label column when deleting columns
-2019-3-10	LC	Fix bug on not deleting the data in column
-*/
+                                    -- smallint
 GO
 
 IF EXISTS
@@ -68,9 +64,9 @@ Parameters
     - Valid Class TableName as a string
     - Pass the class table name, e.g.: 'MFCustomer'
   @columnNames nvarchar(4000)
-    fixme description
+    Name of the column to be removed
   @process\_ID smallint
-    fixme description
+    Use any flag that is not 0 = 4 to indicate the records that should be included. Set the flag on all records if the adhoc property should be removed from all
   @ProcessBatch\_ID int (optional, output)
     Referencing the ID of the ProcessBatch logging table
   @Debug smallint (optional)
@@ -82,17 +78,38 @@ Parameters
 Purpose
 =======
 
+This procedure is specially useful when the metadata design in the vault has changed over time and properties that is not used any longer is still remaining on the metadata card.  Instead of manually deleted these properties from the metadata card, they can be deleted in bulk using the Connector.
+
 Additional Info
 ===============
+
+When a class table is refreshed in SQL and the properties are not defined on the metadata card, but are still on the object then it will be added as a separate column towards the end of the Class Table list of columns.
+
+Using this procedure can delete these columns and delete it from the metadata.
 
 Prerequisites
 =============
 
-Warnings
-========
+There is a few requirements or steps to be taken to use this procedure:
+
+- Identify the adhoc columns towards the end of the Class Table column list.
+- Any column that is not a default column can be specified.
+- The property will only be removed from the metadata card if there are no objects with values for that property any longer.
+- If the property is set on the metadata card it, the value will be set to Null but it will not be removed.
 
 Examples
 ========
+
+.. code:: sql
+
+    DECLARE @return_value int
+    EXEC    @return_value = [dbo].[spMFDeleteAdhocProperty]
+                                  @MFTableName =  N'MFCustomer',
+                                  @columnNames =  N'Address',
+                                  @process_ID = 5,
+                                  @Debug =  NULL
+    SELECT  'Return Value' = @return_value
+    GO
 
 Changelog
 =========
@@ -101,6 +118,8 @@ Changelog
 Date        Author     Description
 ----------  ---------  --------------------------------------------------------
 2019-08-30  JC         Added documentation
+2018-04-25  LC         Fix bug to pick up both ID column and label column when deleting columns
+2019-03-10  LC         Fix bug on not deleting the data in column
 ==========  =========  ========================================================
 
 **rST*************************************************************************/
