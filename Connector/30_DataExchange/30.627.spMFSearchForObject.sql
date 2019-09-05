d4460fe1c73142f5dcd89a758ff713c334e28f6b
Josh de Kock <josh@itanimul.li>
2019-09-05 21:23:17 +0100

    add spMFSearchForObject documentation

--- Connector/30_DataExchange/30.627.spMFSearchForObject.sql
+++ Connector/30_DataExchange/30.627.spMFSearchForObject.sql
@@ -35,11 +35,11 @@ go
 
 ALTER PROCEDURE [dbo].[spMFSearchForObject] (@ClassID     INT
                                               ,@SearchText NVARCHAR (2000)
-                                              ,@Count      INT = 1									
-											  ,@OutputType INT = 0 -- 0 = output to select 1 = output to temp search table
-											  ,@XMLOutPut xml output
-											  ,@TableName varchar(200)='' output
-											  ,@Debug SMALLINT = 0)
+                                              ,@Count      INT = 1
+                                              ,@OutputType INT = 0 -- 0 = output to select 1 = output to temp search table
+                                              ,@XMLOutPut xml output
+                                              ,@TableName varchar(200)='' output
+                                              ,@Debug SMALLINT = 0)
 AS
 /*rST**************************************************************************
 
@@ -52,37 +52,60 @@ Return
   - -1 = Error
 Parameters
   @ClassID int
-    fixme description
+    ID of the class
   @SearchText nvarchar(2000)
-    fixme description
-  @Count int
-    fixme description
+    Pass name of the object for a specific record else pass NULL to get all objects
+  @Count int (optional)
+    - Default = 1
+    - The maximum number of results to return. Specify 0 to return unlimited number of results.
   @OutputType int
-    fixme description
+    - 0 = output to XML (default)
+    - 1 = output to temporary table and update MFSearchLog
   @XMLOutPut xml (output)
-    fixme description
+    Used if outputType = 0 then this parameter returns the result in XML format
   @TableName varchar(200) (output)
-    fixme description
+    Used if outputType = 1 then this parameter returns the name of the temporary file with the result
   @Debug smallint (optional)
     - Default = 0
     - 1 = Standard Debug Mode
     - 101 = Advanced Debug Mode
 
-
 Purpose
 =======
+
+To search for objects with class id and name or title property.
 
 Additional Info
 ===============
 
-Prerequisites
-=============
+This procedure will call “spMFSearchForObjectInternal” and get the objects details that satisfies the search conditions and shows the objects details in tabular format.
 
-Warnings
-========
+The result is either:
+
+- inserted in a temporary table. MFSearchLog is updated with the name of the table and summary of result.
+- output as an XML output parameter.
 
 Examples
 ========
+
+.. code:: sql
+
+    EXEC [dbo].[spMFSearchForObject] @ClassID = 78                  -- the class MFID: this can be obtained from select Name, MFID from MFClass
+                                    ,@SearchText = 'A'              -- any text value, this can be a part text. It does not cater for wildcards
+                                    ,@Count = 5                     -- used to restrict the number of search result returns.
+                                    ,@Debug = 0
+                                    ,@OutputType = 1                -- set to 1 to channel output to a table
+                                    ,@XMLOutPut = @XMLOutPut OUTPUT -- is null 
+                                    ,@TableName = @TableName OUTPUT;
+
+                                                                    -- used in subsequent processing to process the search result.
+    --show temp table name
+    SELECT @TableName AS [TableName];
+
+    --view search result
+    SELECT *
+    FROM [dbo].[MFSearchLog] AS [msl];
+    GO
 
 Changelog
 =========
@@ -91,27 +114,16 @@ Changelog
 Date        Author     Description
 ----------  ---------  --------------------------------------------------------
 2019-08-30  JC         Added documentation
+2019-05-06  LC         Change destination of search to a temporary file
+2018-04-04  DEV2       Added License Module validation code.
+2016-09-26  DEV2       Removed vault settings parameters and pass them as comma separated string in @VaultSettings parameter.
+2016-08-27  LC         Update variabletable function parameters
+2016-08-24  DEV2       TaskID 471
+2016-06-26  LC         Debugging added
+2014-04-29  DEV2       RETURN statement added
 ==========  =========  ========================================================
 
 **rST*************************************************************************/
-
-  /*******************************************************************************
-  ** Desc:  The purpose of this procedure is to search for an object in M-Files  
-  **  
-  ********************************************************************************
-  ** Change History
-  ********************************************************************************
-  ** Date        Author     Description
-  ** ----------  ---------  -----------------------------------------------------
-  ** 29-04-2014  DEV 2      RETURN statement added
-  ** 26-6-2016   LeRoux	Debugging added
-  ** 24-8-2016	 DEV 2		TaskID 471
-  ** 27-8-2016	LC			Update variabletable function parameters
-  ** 26-9-2016  DevTeam2    Removed vault settings parameters and pass them as 
-                            comma separated string in @VaultSettings parameter.
-	 04-04-2018 DevTeam2    Added License Module validation code.
-	 06-5-2019 LC			Change destination of search to a temporary file
-  ******************************************************************************/
   BEGIN
       BEGIN TRY
           BEGIN TRANSACTION
