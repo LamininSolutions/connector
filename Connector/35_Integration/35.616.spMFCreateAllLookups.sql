340065b6ef6a707facba4562c2a8df6308caf5fb
Josh de Kock <josh@itanimul.li>
2019-09-05 17:39:48 +0100

    add spMFCreateAllLookups documentation

--- Connector/35_Integration/35.616.spMFCreateAllLookups.sql
+++ Connector/35_Integration/35.616.spMFCreateAllLookups.sql
@@ -21,7 +21,6 @@ GO
   MODIFICATION HISTORY
   ====================
  	DATE			NAME		DESCRIPTION
-	2019-1-30		LC			Valuelist name: Source Does not exist or is a duplicate
 ------------------------------------------------------------------------------------------------*/
 /*-----------------------------------------------------------------------------------------------
   USAGE:
@@ -79,31 +78,22 @@ Parameters
   @ProcessBatch\_ID int (optional, output)
     Referencing the ID of the ProcessBatch logging table
   @Schema nvarchar(20)
-    fixme description
-  @IncludeInApp int
-    fixme description
-  @WithMetadataSync bit
-    fixme description
+    - Default = 'dbo'
+    - Which schema to operate on
+  @IncludeInApp int (optional)
+    - Default = 1
+  @WithMetadataSync bit (optional)
+    - Default = 0
+    - 1 = call spMFDropAndUpdateMetadata internally
   @Debug smallint (optional)
     - Default = 0
     - 1 = Standard Debug Mode
     - 101 = Advanced Debug Mode
 
-
 Purpose
 =======
 
-Additional Info
-===============
-
-Prerequisites
-=============
-
-Warnings
-========
-
-Examples
-========
+Prodecedure to create all workflow and Valuelist lookups that is used in the included in App class tables
 
 Changelog
 =========
@@ -112,6 +102,7 @@ Changelog
 Date        Author     Description
 ----------  ---------  --------------------------------------------------------
 2019-08-30  JC         Added documentation
+2019-01-30  LC         Valuelist name: Source Does not exist or is a duplicate
 ==========  =========  ========================================================
 
 **rST*************************************************************************/
